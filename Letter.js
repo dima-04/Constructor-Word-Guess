a17c89673d9bd601376e43057f83a92d450a3d38
Dima <dima.dibb1@gmail.com>
2019-10-30 23:35:50 -0700

    letter.js

---
+++ Letter.js
@@ -0,0 +1,19 @@
+function Letter(char){
+    this.char=char;
+    this.guessed=false;
+
+    this.print =function(){
+        if(this.guessed){
+            return this.char;
+        }else{
+            return "_";
+        }
+    }
+    this.guess= function(char){
+       if(this.char===char){
+           this.guessed=true;
+       }
+
+    }
+
+}
